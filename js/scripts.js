5fb92a1803c8ad3acb139dfbe9904fde273aa7c8
gk-5 <gkproctor@gmail.com>
2019-11-18 12:01:50 -0800

    delete unnecessary files

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    var holdWidth = $(window).width();
-    $(window).on('resize', function () {
-        newPercentage = (($(window).width() / holdWidth) * 100) + "%";
-        $("html").css("font-size", newPercentage)
-        
-    });
-});
