d1d0b8d1abc8719b180aebd5fb7ce646b60b83fb
gkproctor <gkproctor@gmail.com>
2021-01-14 08:26:23 -0800

    deleted apple and google pages

--- gatsby/src/pages/appleMaps.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function AppleMapsPage() {
-  return (
-    <>
-      <p>This is the Apple Maps page</p>
-    </>
-  );
-}
--- gatsby/src/pages/googleMaps.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function GoogleMapsPage() {
-  return (
-    <>
-      <p>This is the Google Maps page</p>
-    </>
-  );
-}
